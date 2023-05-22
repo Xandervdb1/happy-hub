674ae09be0e3fe9a59f1bb917c18bb3ede78bcaf
Vandevelde <raoul.vandevelde@gmail.com>
2023-05-22 17:01:23 +0200

    Add seeder and factory FOR User Model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,6 +19,11 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'lastname' => fake()->name(),
+            'username' => fake()->name(),
+            'birthday' => fake()->date(),
+            'function' => fake()->word(),
+            'department' => fake()->word(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\User::factory(10)->create();
     }
 }
