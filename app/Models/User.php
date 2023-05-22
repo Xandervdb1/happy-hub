07d7d5b3ea9a252bbf40e5b2e2e6c89d542a801c
Vandevelde <raoul.vandevelde@gmail.com>
2023-05-22 16:40:56 +0200

    Add relationship FOR User model and Team model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    
+    use HasFactory;
+
+    function team()
+    {
+        return $this->hasOne(Team::class);
+    }
 }
