e948c3f757f00a07dc5c2779876056b76f2f2502
Xandervdb1 <xandervandenbossche0@gmail.com>
2023-05-30 11:28:58 +0200

    Fix global var for logged in user

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Tightenco\Ziggy\Ziggy;
+use Illuminate\Support\Facades\Auth;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -32,19 +33,7 @@ class HandleInertiaRequests extends Middleware
     {
         return array_merge(parent::share($request), [
             'auth' => [
-                'user' => $request->user()->only([
-                    'id',
-                    'name',
-                    'lastname',
-                    'username',
-                    'birthday',
-                    'function',
-                    'coins',
-                    'team_id',
-                    'company_id',
-                    'role_id',
-                    'is_admin'
-                ]),
+                'user' => $request->user(),
             ],
             'ziggy' => function () use ($request) {
                 return array_merge((new Ziggy)->toArray(), [
