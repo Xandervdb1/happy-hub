718ac654925260301a34cb59de9af36d79d9af3f
Sofievdabeele <sofie.vdabeele@gmail.com>
2023-05-31 15:01:38 +0200

    Add some css to AllQuests page

--- resources/js/Pages/userDashboard/AllQuests.jsx
+++ resources/js/Pages/userDashboard/AllQuests.jsx
@@ -10,25 +10,49 @@ const AllQuests = (props) => {
     return (
         <>
             <Header />
-            <table>
-                <tbody>
-                    {userQuests.map((userQuest) => (
-                        <tr key={userQuest.id}>
-                        <td>{userQuest.name}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-<br />
-            <table>
-                <tbody>
-                    {teamQuests.map((teamQuest) => (
-                        <tr key={teamQuest.id}>
-                        <td>{teamQuest.name}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <div className="titleContainer">
+                <p>QUESTS</p>
+            </div>
+            <div className='questContainer'>
+                <p className='title'>Personal Quests</p>
+                <hr className="underline" />
+
+                <div className="personalQuests">
+                    <div className="reward">
+                        <table>
+                            <tbody>
+                                {
+                                    userQuests.map((userQuest) => (
+                                        <tr key={userQuest.id}>
+                                            <td>{userQuest.name}</td>
+                                            <td>{userQuest.coins}</td>
+                                            <img className="coins" src="../coin.png" alt="coin Happy Hub" />
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <br />
+                <div className="teamQuests">
+                    <h1 className="title">Your team quests</h1>
+                    <hr className="underline" />
+                    <div className="questRewards">
+                        <table>
+                            <tbody>
+                                {teamQuests.map((teamQuest) => (
+                                    <tr key={teamQuest.id}>
+                                        <td>{teamQuest.name}</td>
+                                        <td>{teamQuest.coins}</td>
+                                        <img className="coins" src="../coin.png" alt="coin Happy Hub" />
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
