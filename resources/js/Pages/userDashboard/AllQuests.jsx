6cf9803e46e1e0718130feed744056ca767d5a2b
Sofievdabeele <sofie.vdabeele@gmail.com>
2023-05-31 14:12:50 +0200

    Add userquest and teamquest to AllQuestpage

--- resources/js/Pages/userDashboard/AllQuests.jsx
+++ resources/js/Pages/userDashboard/AllQuests.jsx
@@ -1,13 +1,34 @@
 import Header from "@/Components/Header";
 
 const AllQuests = (props) => {
-    console.log(props.userQuests);
-    console.log(props.teamQuests);
+    // console.log(props.userQuests.name);
+    // console.log(props.teamQuests);
+    
+    const { userQuests, teamQuests } = props;
+   
 
     return (
         <>
             <Header />
-           
+            <table>
+                <tbody>
+                    {userQuests.map((userQuest) => (
+                        <tr key={userQuest.id}>
+                        <td>{userQuest.name}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+<br />
+            <table>
+                <tbody>
+                    {teamQuests.map((teamQuest) => (
+                        <tr key={teamQuest.id}>
+                        <td>{teamQuest.name}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
     )
 }
