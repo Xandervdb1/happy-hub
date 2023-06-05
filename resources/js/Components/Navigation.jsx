3d8f6cdee3f3329848809d37957e76f6f9f42e14
Nielsington <declercqniels@hotmail.com>
2023-06-05 17:14:29 +0200

    Add navigation component

---
+++ resources/js/Components/Navigation.jsx
@@ -0,0 +1,21 @@
+import { Link } from "@inertiajs/react";
+
+const Navigation = () => {
+
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/generate-key">Generate Key</Link></li>
+                    <li><Link href="/register">Register</Link></li>
+                    <li><Link href="/company-dashboard">Admin Dashboard</Link></li>
+                    <li><Link href="/user-dashboard">User Dashboard</Link></li>
+                    <li><Link href="/wallet">Wallet</Link></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Navigation;
