ae5f2737aadf80ec45a52074e51e295608fdcf31
Xandervdb1 <xandervandenbossche0@gmail.com>
2023-06-06 11:42:35 +0200

    Change nav links and render based on whether someone is logged in and/or admin or not

--- resources/js/Components/Navigation.jsx
+++ resources/js/Components/Navigation.jsx
@@ -1,21 +1,45 @@
-import { Link } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 
 const Navigation = () => {
-
-    return (
-        <>
-            <nav>
-                <ul>
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/generate-key">Generate Key</Link></li>
-                    <li><Link href="/register">Register</Link></li>
-                    <li><Link href="/company-dashboard">Admin Dashboard</Link></li>
-                    <li><Link href="/user-dashboard">User Dashboard</Link></li>
-                    <li><Link href="/wallet">Wallet</Link></li>
-                </ul>
-            </nav>
-        </>
-    )
+    const props = usePage().props;
+    if (props.auth.user && props.auth.user.is_admin) {
+        return (
+            <>
+                <nav>
+                    <ul>
+                        <li><Link href="/user-dashboard">User Dashboard</Link></li>
+                        <li><Link href="/company-dashboard">Company Dashboard</Link></li>
+                        <li><Link href="/wallet">Wallet</Link></li>
+                        <li><Link href="/logout" method="post" as="button">Log out</Link></li>
+                    </ul>
+                </nav>
+            </>
+        )
+    } else if (props.auth.user) {
+        return (
+            <>
+                <nav>
+                    <ul>
+                        <li><Link href="/user-dashboard">User Dashboard</Link></li>
+                        <li><Link href="/wallet">Wallet</Link></li>
+                        <li><Link href="/logout" method="post" as="button">Log out</Link></li>
+                    </ul>
+                </nav>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <nav>
+                    <ul>
+                        <li><Link href="/">Home</Link></li>
+                        <li><Link href="/login">Log in</Link></li>
+                        <li><Link href="/generate-key">Buy key</Link></li>
+                    </ul>
+                </nav>
+            </>
+        )
+    }
 }
 
 export default Navigation;
