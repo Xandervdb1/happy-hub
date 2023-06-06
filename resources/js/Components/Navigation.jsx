7669083b76384cdb5367dec15e32b34d6a5be3c2
Xandervdb1 <xandervandenbossche0@gmail.com>
2023-06-06 11:50:12 +0200

    Refactor nav code into single if statement

--- resources/js/Components/Navigation.jsx
+++ resources/js/Components/Navigation.jsx
@@ -2,25 +2,13 @@ import { Link, usePage } from "@inertiajs/react";
 
 const Navigation = () => {
     const props = usePage().props;
-    if (props.auth.user && props.auth.user.is_admin) {
+    if (props.auth.user) {
         return (
             <>
                 <nav>
                     <ul>
                         <li><Link href="/user-dashboard">User Dashboard</Link></li>
-                        <li><Link href="/company-dashboard">Company Dashboard</Link></li>
-                        <li><Link href="/wallet">Wallet</Link></li>
-                        <li><Link href="/logout" method="post" as="button">Log out</Link></li>
-                    </ul>
-                </nav>
-            </>
-        )
-    } else if (props.auth.user) {
-        return (
-            <>
-                <nav>
-                    <ul>
-                        <li><Link href="/user-dashboard">User Dashboard</Link></li>
+                        {props.auth.user.is_admin ? <li><Link href="/company-dashboard">Company Dashboard</Link></li> : ""}
                         <li><Link href="/wallet">Wallet</Link></li>
                         <li><Link href="/logout" method="post" as="button">Log out</Link></li>
                     </ul>
