c9b63439d36693b98c84620613c27fde185ae530
Xandervdb1 <xandervandenbossche0@gmail.com>
2023-06-06 12:35:56 +0200

    Fix return in map function for all quests

--- resources/js/Pages/userDashboard/AllQuests.jsx
+++ resources/js/Pages/userDashboard/AllQuests.jsx
@@ -3,10 +3,9 @@ import quests from "./questcss/quests.css";
 import ProgressBar from "@/Components/ProgressBar";
 
 const AllQuests = (props) => {
-    
+
     const { userQuests, teamQuests, userCoins, questCoins } = props;
     console.log(props.userCoins);
-    console.log(props.questCoins);
 
     // const userQuests = props.userQuests;
     // const teamQuests = props.teamQuests;
@@ -15,41 +14,43 @@ const AllQuests = (props) => {
     // const questCoins = props.questCoins;
 
 
-    
+
     const bgColor = '#393E46';
-    
+
     // const testData = 
     // { bgcolor: "#393E46", completed: 60 };
-    
+
     return (
         <>
             <Header />
             <div className="QuestContainer">
                 <h1>QUESTS</h1>
-                
+
                 <div className="personalQuestsContainer" id='1'>
                     <p className='title'>Personal Quests</p>
                     <div className="personalQuests">
                         {
+
                             userQuests.map((userQuest) => {
-                                const completed = (parseInt(userCoins) / parseInt(questCoins)) *100;
-                                <div className="personalQuest" key={userQuest.id}>
-                                    <div className="firstRow">
-                                        <p className="name">{userQuest.name}</p>
-                                        <div className="column">
-                                            <p className="questCoins">{userQuest.coins}</p>
-                                            <img className="coins" src="../coin.png" alt="coin Happy Hub" />
-                                            <br />
+                                const completed = (parseInt(userCoins) / parseInt(userQuest.coins)) * 100;
+                                return (
+                                    <div className="personalQuest" key={userQuest.id}>
+                                        <div className="firstRow">
+                                            <p className="name">{userQuest.name}</p>
+                                            <div className="column">
+                                                <p className="questCoins">{userQuest.coins}</p>
+                                                <img className="coins" src="../coin.png" alt="coin Happy Hub" />
+                                                <br />
+                                            </div>
                                         </div>
+                                        <ProgressBar bgcolor={bgColor} completed={completed} />
                                     </div>
-                                    <ProgressBar bgcolor={bgColor} completed={completed} />
-                                    { console.log(completed)}
-                                </div>
+                                )
                             })
                         }
                     </div>
                 </div>
-                
+
                 <div className="teamQuestsContainer" id='2'>
                     <p className="title">Your team quests</p>
                     <div className="teamQuests">
@@ -64,13 +65,13 @@ const AllQuests = (props) => {
                                             <br />
                                         </div>
                                     </div>
-                                    <ProgressBar bgcolor={bgColor} completed={completed} />
+                                    {/* <ProgressBar bgcolor={bgColor} completed={completed} /> */}
                                 </div>
                             ))
-                        }       
+                        }
                     </div>
                 </div>
-            </div> 
+            </div>
         </>
     )
 }
