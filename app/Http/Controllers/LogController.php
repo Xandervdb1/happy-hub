a30c9a8698d8cf22c8e49792789dcb296084d660
Vandevelde <raoul.vandevelde@gmail.com>
2023-06-09 14:11:34 +0200

    Add show all for Logs Controller

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Log;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
 
 class LogController extends Controller
 {
@@ -18,5 +20,17 @@ class LogController extends Controller
         $log->company_id = 1;
 
         $log->save();
+    }
+
+    function showAll()
+    {
+        // Get the currently logged-in user
+        $user = Auth::user();
+
+        // Retrieve all logs associated with the user
+        $logs = Log::where('company_id', $user->company_id)->get();
+
+        // Return the logs to the view or perform any other actions
+        return Inertia::render('logs', ['logs' => $logs]);
     }
 }
