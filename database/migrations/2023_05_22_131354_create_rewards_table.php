c3de956a1281e1fb6af4ff1914aef1932410f53b
Xandervdb1 <xandervandenbossche0@gmail.com>
2023-06-13 15:44:40 +0200

    Add reward logic for claiming

--- database/migrations/2023_05_22_131354_create_rewards_table.php
+++ database/migrations/2023_05_22_131354_create_rewards_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->integer('price');
             $table->string('slug');
+            $table->string('type')->nullable();
             $table->timestamps();
         });
     }
