1d974fbeeba0d23ead2cd8613c72f81968044d4e
JitskeDeherdt <jitske.123@hotmail.com>
2023-06-13 11:38:39 +0200

    Fix double rewards

--- resources/js/Components/Reward.jsx
+++ resources/js/Components/Reward.jsx
@@ -16,15 +16,6 @@ const Reward = (props) => {
                     </div>
                 </div>
             </Link>
-            <Link as="button" method='post' id='reward' className={disabled ? 'reward disabled' : 'reward'} key={reward.name + reward.id}>
-                <div className="totalReward">
-                    <p className='activity'> {reward.slug}</p>
-                    <div className="priceContainer">
-                        <img className="coins" src="../coin.png" alt="coin Happy Hub" />
-                        <p className="price">{reward.price.toLocaleString()}</p>
-                    </div>
-                </div>
-            </Link>
         </>
     )
 }
--- resources/js/Pages/wallet/Wallet.jsx
+++ resources/js/Pages/wallet/Wallet.jsx
@@ -1,22 +1,17 @@
 import Header from "@/Components/Header.jsx";
 
-
 const Wallet = (props) => {
-    // console.log(props.userCoins);
-    // console.log(props.teamCoins);
     console.log(props.logs);
     const userCoins = props.userCoins;
     const teamCoins = props.teamCoins;
     const userLogs = props.logs;
-
-
 
     return (
         <>
             <Header userCoins={userCoins} />
             <div className="walletContainer">
                 <div className="titleBalance">
-                    <h1 className="titlePage"> Your Wallet</h1>
+                    <h1 className="titlePage">Your Wallet</h1>
                 </div>
                 <div className="personalBalance">
                     <div className="personalWallet">
@@ -26,37 +21,27 @@ const Wallet = (props) => {
                             <p>{userCoins}</p>
                         </div>
                     </div>
-                    {
-
-                        userLogs.map(userLog => {
-                            if (userLog.type === "Personal") {
-                                if (userLog.scope === "Reward") {
-                                    return (
-                                        <>
-                                            <div className="scope">
-                                                <i><li className="timestamp">
-                                                    {userLog.created_at}</li></i>
-                                                <p className="red">Spent <b> {userLog.scopeCoins} </b>coins on <b>{userLog.scopeName}</b></p>
-                                                <p className="coinTotal">(Total coins: {userCoins.toLocaleString()} coins.)</p>
-                                            </div>
-                                        </>
-                                    )
-                                } else {
-                                    return (
-                                        <>
-                                            <div className="scope">
-                                                <i><li className="timestamp"> {userLog.created_at}</li></i>
-                                                <p className="green">Gained <b>{userLog.scopeCoins.toLocaleString()} </b> for <b> {userLog.scopeName}.</b></p>
-                                                <p className="coinTotal">(Total coins:{userCoins.toLocaleString()}) </p>
-                                            </div>
-
-                                        </>
-                                    )
-
-                                }
+                    {userLogs.map(userLog => {
+                        if (userLog.type === "Personal") {
+                            if (userLog.scope === "Reward") {
+                                return (
+                                    <div className="scope" key={userLog.created_at}>
+                                        <i><li className="timestamp">{userLog.created_at}</li></i>
+                                        <p className="red">Spent <b>{userLog.scopeCoins}</b> coins on <b>{userLog.scopeName}</b></p>
+                                        <p className="coinTotal">(Total coins: {userCoins.toLocaleString()} coins.)</p>
+                                    </div>
+                                );
+                            } else {
+                                return (
+                                    <div className="scope" key={userLog.created_at}>
+                                        <i><li className="timestamp">{userLog.created_at}</li></i>
+                                        <p className="green">Gained <b>{userLog.scopeCoins.toLocaleString()}</b> for <b>{userLog.scopeName}.</b></p>
+                                        <p className="coinTotal">(Total coins: {userCoins.toLocaleString()} coins.)</p>
+                                    </div>
+                                );
                             }
-                        })
-                    }
+                        }
+                    })}
                 </div>
 
                 <div className="teamBalance">
@@ -64,39 +49,34 @@ const Wallet = (props) => {
                         <h1 className="titleWallet">Team Balance</h1>
                         <div className="pricesAndCoin">
                             <img className="coin" src="../coin.png" alt="coin" />
-                            <p>{teamCoins}</p>
+                            <p>{teamCoins.toLocaleString()}</p>
                         </div>
-
                     </div>
-                    <div className="transactions">
-                        <p className="timestamp">17/05/2023 - 12:56</p>
-                        <div className="priceAndCoin">
-                            <p>125213500</p>
-                            <img className="coin" src="../coin.png" alt="coin" />
-                        </div>
-                        <p className="activity">Help a colleague</p>
-                    </div>
-
-
+                    {userLogs.map(userLog => {
+                        if (userLog.type === 'Team') {
+                            if (userLog.scope === "Reward") {
+                                return (
+                                    <div className="scope" key={userLog.created_at}>
+                                        <i><li className="timestamp">{userLog.created_at}</li></i>
+                                        <p className="red">Spent <b>{userLog.scopeCoins}</b> coins on <b>{userLog.scopeName}</b></p>
+                                        <p className="coinTotal">(Total coins: {teamCoins.toLocaleString()} coins.)</p>
+                                    </div>
+                                );
+                            } else {
+                                return (
+                                    <div className="scope" key={userLog.created_at}>
+                                        <i><li className="timestamp">{userLog.created_at}</li></i>
+                                        <p className="green">Gained <b>{userLog.scopeCoins.toLocaleString()}</b> for <b>{userLog.scopeName}.</b></p>
+                                        <p className="coinTotal">(Total coins: {teamCoins.toLocaleString()} coins.)</p>
+                                    </div>
+                                );
+                            }
+                        }
+                    })}
                 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             </div>
-
         </>
-    )
-}
-export default Wallet
+    );
+};
+
+export default Wallet;
