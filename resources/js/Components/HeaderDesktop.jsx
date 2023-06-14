dffbd4d1306e956053619c9a2d452fb1a2aa16e9
Xandervdb1 <xandervandenbossche0@gmail.com>
2023-06-14 15:09:19 +0200

    Add header showing links based on login or not

--- resources/js/Components/HeaderDesktop.jsx
+++ resources/js/Components/HeaderDesktop.jsx
@@ -1,30 +1,47 @@
-import { Link } from "@inertiajs/react"
+import { Link, usePage } from "@inertiajs/react"
 
 
-const HeaderDesktop = (props) => {
+const HeaderDesktop = () => {
+    const props = usePage().props;
+    let userCoins;
+    if (props.auth.user) {
+        userCoins = props.auth.user.coins;
+    }
     return (
         <>
             <div className="HeaderContainer" id='Desktop'>
                 <header>
                     <img className='logoHeader' src="../happyhub.png" alt="logo Happy Hub" />
                     <div className="navigation">
-                        <Link href='/company-dashboard' className="navLink">Company Dashboard</Link>
-                        <Link href='/user-dashboard' className="navLink">User Dashboard</Link>
-                        <Link href='/wallet' className="navLink"> Wallet</Link>
-                        <Link href='/logout' method="post" as="button" className="navLink"> Log out</Link>
+                        {props.auth.user ? (
+                            <>
+                                <Link href='/user-dashboard' className="navLink">User Dashboard</Link>
+                                {props.auth.user.is_admin ? <Link className="navLink" href="/company-dashboard">Company Dashboard</Link> : ""}
+                                <Link href='/wallet' className="navLink"> Wallet</Link>
+                                <Link href='/logout' method="post" as="button" className="navLink"> Log out</Link>
+                            </>
+                        ) : (
+                            <>
+                                <Link className="navLink" href="/">Home</Link>
+                                <Link className="navLink" href="/login">Log in</Link>
+                                <Link className="navLink" href="/generate-key">Buy key</Link>
+                            </>
+                        )}
                     </div>
                     <div className="walletContainer">
-                        <p className="wallet">{props.userCoins ? props.userCoins : 0}</p>
-                        <img className='coinHeader' src="../coin.png" ahref='' lt="coin Happy Hub" />
+                        {props.auth.user ? (
+                            <>
+                                <p className="wallet">{userCoins ? userCoins : 0}</p>
+                                <img className='coinHeader' src="../coin.png" ahref='' lt="coin Happy Hub" />
+                            </>
+                        ) : (
+                            <div></div>
+                        )}
                     </div>
 
                 </header >
             </div>
         </>
-
-
-
-
     )
 };
 export default HeaderDesktop;
