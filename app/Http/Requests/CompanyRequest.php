8ab20208f931c5ed32ff58dec1a109773620bfbd
Vandevelde <raoul.vandevelde@gmail.com>
2023-06-14 11:36:33 +0200

    Update error messages for form validation and zip requirements are longer

--- app/Http/Requests/CompanyRequest.php
+++ app/Http/Requests/CompanyRequest.php
@@ -28,7 +28,7 @@ class CompanyRequest extends FormRequest
             'companyname' => 'required|unique:companies,name|min:1|max:255',
             'address' => 'required|min:1|max:255',
             'addressnumber' => 'required|min:1|max:4',
-            'zip' => 'required|min:1|max:4',
+            'zip' => 'required|min:1|max:8',
             'city' => 'required|max:255',
             'country' => 'required|max:255',
             'vatnumber' => 'required|regex:/^[A-Z]{2}/'
@@ -38,7 +38,7 @@ class CompanyRequest extends FormRequest
     public function messages()
     {
         return [
-            'required' => 'Please provide a :attribute',
+            'required' => 'Please provide :attribute',
             'min' => ':attribute must be longer',
             'unique' => ':attribute already exists',
             'max' => ':attribute cannot exceed 255 characters',
