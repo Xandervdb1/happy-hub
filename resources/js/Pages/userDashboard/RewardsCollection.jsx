953c9f62105be07f5d71c9883db772f92f0728ff
Xandervdb1 <xandervandenbossche0@gmail.com>
2023-06-16 12:55:33 +0200

    Add extra validation and messages and style rewards page and Fix back buttons

--- resources/js/Pages/userDashboard/RewardsCollection.jsx
+++ resources/js/Pages/userDashboard/RewardsCollection.jsx
@@ -2,6 +2,7 @@ import Header from '@/Components/HeaderMobile';
 import HeaderDesktop from "@/Components/HeaderDesktop.jsx"
 import Reward from '@/Components/Reward';
 import { Link } from '@inertiajs/react';
+// import { useNavigate } from 'react-router-dom';
 // import '../../css/RewardsCollection.scss';
 
 //TODO: change .personalRewards,.teamRewards,.disabledPersonalRewards,.disabledTeamRewards to 1 general class (rewardsWrap?)
@@ -13,65 +14,73 @@ const RewardsCollection = (props) => {
     const teamWallet = props.teamCoins;
     const userCoins = props.userCoins;
 
+    const goBackHandler = () => {
+        window.history.back();
+    }
+
     return (
         <>
             <HeaderDesktop userCoins={userCoins} />
             <Header userCoins={userCoins} />
-            <div className='titleContainer'>
-                <p>REWARDS</p>
-            </div>
-            <div className='rewardsContainerWrap'>
-                <div className='rewardsContainer'>
-                    <p className='title'>Personal</p>
-                    <div className='personalRewards'>
-                        {
-                            userRewards.map((reward, index) => (
-                                personalWallet >= reward.price ? (
-                                    <Reward reward={reward} disabled={false} key={reward + index} />
-                                ) : null
-                            ))
-                        }
-                    </div>
+            <div className="rewardsContainerParent">
+                <div className='titleContainer'>
+                    <p>REWARDS</p>
                 </div>
-                <div className='rewardsContainer'>
-                    <p className='title'>Team</p>
-                    <div className="teamRewards">
-                        {
-                            teamRewards.map((reward, index) => (
-                                teamWallet >= reward.price ? (
-                                    <Reward reward={reward} disabled={false} key={reward + index} />
-                                ) : null
-                            ))
-                        }
-                    </div>
-                </div>
-                <div className='rewardsContainer'>
-                    <p className='title'>Earn more, unlock bigger</p>
-                    <div className="disabledRewards">
-                        <div className='disabledPersonalRewards'>
-                            <p className='subTitle'>Personal</p>
+                <div className='rewardsContainerWrap'>
+                    <div className='rewardsContainer'>
+                        <p className='title'>Personal</p>
+                        <div className='personalRewards'>
                             {
                                 userRewards.map((reward, index) => (
-                                    personalWallet < reward.price ? (
-                                        <Reward reward={reward} class='disabled' disabled={true} key={reward + index} />
-                                    ) : null
-                                ))
-                            }
-                        </div>
-                        <div className='disabledTeamRewards'>
-                            <p className='subTitle'>Team</p>
-                            {
-                                teamRewards.map((reward, index) => (
-                                    teamWallet < reward.price ? (
-                                        <Reward reward={reward} class='disabled' disabled={true} key={reward + index} />
+                                    personalWallet >= reward.price ? (
+                                        <Reward reward={reward} disabled={false} key={reward + index} />
                                     ) : null
                                 ))
                             }
                         </div>
                     </div>
+                    <div className='rewardsContainer'>
+                        <p className='title'>Team</p>
+                        <div className="teamRewards">
+                            {
+                                teamRewards.map((reward, index) => (
+                                    teamWallet >= reward.price ? (
+                                        <Reward reward={reward} disabled={false} key={reward + index} />
+                                    ) : null
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <div className='rewardsContainer'>
+                        <p className='title'>Earn more, unlock bigger</p>
+                        <div className="disabledRewards">
+                            <div className='disabledPersonalRewards'>
+                                <p className='subTitle'>Personal</p>
+                                {
+                                    userRewards.map((reward, index) => (
+                                        personalWallet < reward.price ? (
+                                            <Reward reward={reward} class='disabled' disabled={true} key={reward + index} />
+                                        ) : null
+                                    ))
+                                }
+                            </div>
+                            <div className='disabledTeamRewards'>
+                                <p className='subTitle'>Team</p>
+                                {
+                                    teamRewards.map((reward, index) => (
+                                        teamWallet < reward.price ? (
+                                            <Reward reward={reward} class='disabled' disabled={true} key={reward + index} />
+                                        ) : null
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="goBackButtonContainer">
+                    <Link onClick={goBackHandler} className="goBackButton button">Go back</Link>
                 </div>
             </div>
-            <Link href='/user-dashboard' className="goBackButton">Go back</Link>
         </>
     )
 }
